3030aa53f012b880f274e7fa5f37d7a0f31d1339
Daiane Mainardi <daimainardi@hotmail.com>
2023-08-07 17:29:19 -0300

    Projeção dos dados do author com DTO

--- src/main/java/com/daimainardi/workshopmongo/config/Instantiation.java
+++ src/main/java/com/daimainardi/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.daimainardi.workshopmongo.config;
 
 import com.daimainardi.workshopmongo.domain.Post;
 import com.daimainardi.workshopmongo.domain.User;
+import com.daimainardi.workshopmongo.dto.AuthorDTO;
 import com.daimainardi.workshopmongo.repository.PostRepository;
 import com.daimainardi.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,8 +36,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(List.of(maria, alex,bob));
 
-        Post post1 = new Post(null, sdf.parse("05/08/2023"), "Partiu viagem", "Vou viajar para Paris. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("07/08/2023"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, sdf.parse("05/08/2023"), "Partiu viagem", "Vou viajar para Paris. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("07/08/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(List.of(post1,post2));
 
